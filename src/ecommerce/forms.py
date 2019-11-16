8262661841e1245e73abf361027cfee2438531c6
JulioCantu <32853629+JulioCantu@users.noreply.github.com>
2019-11-16 17:30:44 -0600

    Noviembre

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ContactForm(forms.Form):
+    Nombre = forms.CharField(       
+            widget=forms.TextInput(
+                    attrs={
+                        "class": "form-control", 
+                        "placeholder": "Ingresa tu nombre"
+                    }
+                    )
+            )
+    Correo    = forms.EmailField(       
+            widget=forms.EmailInput(
+                    attrs={
+                        "class": "form-control", 
+                        "placeholder": "Ingresa tu correo"
+                    }
+                    )
+            )
+    Contenido  = forms.CharField(       
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    "placeholder": "Ingresa tu mensaje" 
+                    }
+                )
+            )
+
+
+
+
+
+
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     if not "gmail.com" in email:
+    #         raise forms.ValidationError("Email has to be gmail.com")
+    #     return email
+
+    # def clean_content(self):
+    #     raise forms.ValidationError("Content is wrong.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
